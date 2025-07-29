52fa566392c6911b25d4433aadf210ab66bd2a40
Jacob Bartlett <jacob@lapse.com>
2025-07-29 08:20:26 +0100

    Initial Commit

---
+++ PerformanceAnalytics/PerformanceAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerformanceAnalyticsApp.swift
+//  PerformanceAnalytics
+//
+//  Created by Jacob Bartlett on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PerformanceAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
